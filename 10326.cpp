d6f894574e6a5b3dbba33c2cff549561da6d24d7
asd7766zxc <42734628+asd7766zxc@users.noreply.github.com>
2021-06-07 18:14:20 +0800

    Update 10326.cpp

--- 10326.cpp
+++ 10326.cpp
@@ -69,7 +69,46 @@ int main()
 		}
 		if ( key == map[middle][0] )
 		{
-			cout << "找到ㄌ 在整理後陣列ㄉ " << middle<<" 且在原陣列ㄉ "<< map[middle][1];
+				
+			int fpx = 0; //同時延伸數 
+		//左右延伸數
+			int lpx=1; 
+			int rpx=1; 
+			 	cout << "找到ㄌ (數值,整理後陣列位置,原陣列位置) >>"<<endl;
+			 	
+			for( ; rpx + middle < n &&   middle-lpx > -1 ; fpx++ ) // check if 那啥有重複 
+			{
+				if(key != map[middle+rpx ][0] && key!=map[ middle-lpx][0] )
+				{
+					rpx--;
+					lpx--;
+					break;
+				}
+				if(fpx != 0 )
+				{
+				
+				
+					if (key == map[middle +rpx][0] ) 
+					{
+						cout << "("<<map[middle+rpx][0]<<","<<middle+rpx<<","<<map[middle+rpx][1]<<")"<<endl;
+						rpx++;
+					}
+					if (key == map[middle -lpx][0] )
+					{
+						cout << "("<<map[middle+lpx][0]<<","<<middle+lpx<<","<<map[middle+lpx][1]<<")"<<endl;
+						 lpx++;
+					}	
+				}
+				else 
+				{
+					cout << "("<<map[middle][0]<<","<<middle<<","<<map[middle][1]<<")"<<endl;
+				}
+			
+		   }
+		   //將判斷數分成左右的原因為 fpx = 左界|lpx  middle rpx    |右界 因此 在排列後的陣列中 可以輕易ㄉ找出連續的結果  
+		  
+	
+		   
 			middle = -1;
 			break;
 		}	 	
